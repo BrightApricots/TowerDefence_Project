fd737577d796f0c70f53f88c6b0d6df8ff223b66
wkdtsdndlqslek <ehdtk456@naver.com>
2024-12-10 11:54:40 +0900

    AltarScene 끝

---
+++ TowerDefence/Assets/3.Scripts/UI/UI_AltarPanel.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_AltarPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public int EmberPrice;
+    public int HpPrice;
+    public GameObject Hilight;
+    public GameObject Outline;
+    public GameObject CantBuy;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Hilight.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Hilight.SetActive(false);
+    }
+
+    private void Start()
+    {
+        if(EmberPrice>GameManager.Instance.CurrentEmber|| HpPrice > GameManager.Instance.CurrentHp)
+        {
+            print("못삼");
+            Outline.SetActive(false);
+            CantBuy.SetActive(true);
+        }
+    }
+
+}
